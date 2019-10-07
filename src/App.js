3ba00f12884cf51151faf3988f549e384c9445e6
umekow <umekolashun@gmail.com>
2019-10-07 16:59:22 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 //context objects
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext'; 
 
 function App() {
 	const [products] = useState(data);
@@ -22,18 +23,20 @@ function App() {
 	return (
 	
 		<ProductContext.Provider value={{products, addItem}}>
-			<div className="App">
-				<Navigation cart={cart} />
+			<CartContext.Provider value={cart}>
+				<div className="App">
+					<Navigation cart={cart} />
 
-				{/* Routes */}
-				<Route exact path="/" component={Products} />	)}
-				/>
+					{/* Routes */}
+					<Route exact path="/" component={Products} />	)}
+					/>
 
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
-			</div>
+					<Route
+						path="/cart"
+						component={ShoppingCart}
+					/>
+				</div>
+			</CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
